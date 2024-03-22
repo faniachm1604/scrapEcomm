ce47ca813818300a0b8f70bee28df2ef750beffb
unknown <faniachm1604@gmail.com>
2024-03-22 14:31:28 +0700

    Update , Masih ada Error di Line 51

--- app.js
+++ app.js
@@ -4,11 +4,31 @@ const puppeteer = require("puppeteer");
 const randomUseragent = require("random-useragent");
 
 (async () => {
-  // const url =
-  // "https://shopee.co.id/search?keyword=redmi%20note%205a&is_from_login=true";
-  //   const url = "https://www.tokopedia.com/search?st=product&q=laptop";
-  const url =
-    "https://www.bukalapak.com/products?from=omnisearch&from_keyword_history=false&search%5Bkeywords%5D=sepatu&search_source=omnisearch_keyword&source=navbar";
+  var namaEcomm = "Tokopedia";
+  var url = "";
+
+  switch (namaEcomm) {
+    case "Bukalapak":
+      InspectMasterProduct = ".bl-flex-item.bl-product-list-wrapper";
+      InspectMasterListProduct =
+        '[class="te-product-card bl-product-card-new"]';
+      InspectNamaProduct =
+        ".bl-text.bl-text--body-14.bl-text--secondary.bl-text--ellipsis__2";
+      InspectHargaProduct =
+        ".bl-text.bl-text--semi-bold.bl-text--ellipsis__1.bl-product-card-new__price";
+      InspectLinkProduct = "a[href]";
+      url =
+        "https://www.bukalapak.com/products?from=omnisearch&from_keyword_history=false&search%5Bkeywords%5D=sepatu&search_source=omnisearch_keyword&source=navbar";
+      break;
+    case "Tokopedia":
+      InspectMasterProduct = "body";
+      InspectMasterListProduct = '[data-testid="master-product-card"]';
+      InspectNamaProduct = "[data-testid='spnSRPProdName']";
+      InspectHargaProduct = "[data-testid='spnSRPProdPrice']";
+      InspectLinkProduct = "a[href]";
+      url = "https://www.tokopedia.com/search?st=product&q=laptop";
+      break;
+  }
   const randomAgent = randomUseragent.getRandom();
   //   const browser = await puppeteer.launch({
   //     args: ["--no-sandbox", "--disable-setuid-sandbox"],
@@ -16,42 +36,43 @@ const randomUseragent = require("random-useragent");
   const browser = await puppeteer.launch({
     headless: false,
     // userDataDir: "/tmp/myChromeSession",
-    // executablePath:
-    //   "C:\\Users\\USER\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe",
-    args: ["--incognito"],
+    executablePath:
+      "C:\\Users\\USER\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe",
+    // args: ["--incognito"],
   });
-  // const context = await browser.defaultBrowserContext(); //mode penyamaran
-  //   const context = await browser.defaultBrowserContext(); //mode penyamaran
-  const context = await browser.createIncognitoBrowserContext(); //mode penyamaran
-  const page = await context.newPage(); //membuat tab baru
+  const page = await browser.newPage(); //membuat tab baru
   // const page = await context.newPage(); //membuat tab baru
-
   await page.setViewport({ width: 1080, height: 1024 });
   await page.setJavaScriptEnabled(true); //aktifkan javascript
   await page.setUserAgent(randomAgent); //setting user agent
-  await page.goto(url, { waituntil: "domcontentloaded", timeout: 0 }); //tunggu proses dom/load pagenya selesai
+
+  // console.log(InspetMasterProduct);
+
+  try {
+    await page.goto(url, {
+      waituntil: "domcontentloaded",
+      timeout: 10000,
+    });
+  } catch (e) {
+    console.log("Closed");
+    console.error("Timeout error", error);
+  }
+
+  // InspectMasterProduct = "123";
   const body = await page.evaluate(() => {
-    // return document.querySelector("body").innerHTML;
-    return document.querySelector(".bl-flex-item.bl-product-list-wrapper")
-      .innerHTML;
-  }); //mendapatkan isi tag html body
+    return document.querySelector("body").innerHTML;
+  });
 
   const $ = cheerio.load(body);
-  const listItems = $('[class="te-product-card bl-product-card-new"]');
+  const listItems = $(InspectMasterListProduct);
   var resulst = [];
   listItems.each(function (idx, el) {
-    var nama = $(
-      ".bl-text.bl-text--body-14.bl-text--secondary.bl-text--ellipsis__2",
-      el
-    ).text();
-    var harga = $(
-      ".bl-text.bl-text--semi-bold.bl-text--ellipsis__1.bl-product-card-new__price",
-      el
-    ).text();
-    var link = $("a[href]", el).attr("href");
+    var nama = $(InspectNamaProduct, el).text();
+    var harga = $(InspectHargaProduct, el).text();
+    var link = $(InspectLinkProduct, el).attr("href");
     if (harga != null && harga != "") {
       resulst.push({
-        ecomm: "Bukalapak",
+        ecomm: namaEcomm,
         nama: nama.trim().replace(/[\n\.\+]/g, ""),
         harga: harga.trim().replace(/[\n\.]/g, ""),
         link: link,
@@ -59,25 +80,7 @@ const randomUseragent = require("random-useragent");
     }
   });
 
-  //   const $ = cheerio.load(body);
-  //   const listItems = $('[data-testid="master-product-card"]');
-  //   var resulst = [];
-  //   listItems.each(function (idx, el) {
-  //     var nama = $('[data-testid="spnSRPProdName"]', el).text();
-  //     var harga = $('[data-testid="spnSRPProdPrice"]', el).text();
-  //     var link = $("a[href]", el).attr("href");
-  //     if (harga != null && harga != "") {
-  //       resulst.push({
-  //         nama: nama,
-  //         harga: harga,
-  //         link: link,
-  //       });
-  //     }
-  //   });
-  // console.dir(listItems);
   console.dir(resulst);
-
-  // console.log(body);
 
   await browser.close(); //close browser puppeteer jika sudah selesai
 })();
